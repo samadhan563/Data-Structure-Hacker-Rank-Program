070b557384b550a76558a06dcfb05d2a360b0d9f
Samadhan Nana Gaikwad <73218474+samadhan563@users.noreply.github.com>
2020-12-22 13:09:23 +0530

    Create BigSorting.java

---
+++ BigSorting.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution 
+{
+    public static int com(String s1, String s2)
+    {
+        if(s1.length()==s2.length())
+            {
+            return s1.compareTo(s2);
+            }
+        if(s1.length() > s2.length())
+        {
+           return (1);    
+        }
+        else 
+            {
+            return (-1);
+        }
+    }    
+  
+   public static int partition(String arr[],int start,int end)
+   {
+        int i,piv=start,j=start+1;
+        String k;
+        for(i=start+1;i<=end;i++)
+        {
+          if(Solution.com(arr[i],arr[piv])<0)
+          {
+               k=arr[i];
+               arr[i]=arr[j];
+               arr[j]=k;
+               j+=1;
+          }
+        }
+        k = arr[piv];
+        arr[piv] = arr[j-1];
+        arr[j-1]=k;
+        return (j-1);
+   } 
+    
+   public static void quicksort(String[] arr,int start,int end)
+  {
+       int k,i;
+       if(start<end)
+       {    k = Solution.partition(arr,start,end);
+
+        Solution.quicksort(arr,start,k-1);
+
+        Solution.quicksort(arr,k+1,end);
+
+       }
+  }
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        String[] unsorted = new String[n];
+        for(int unsorted_i=0; unsorted_i < n; unsorted_i++)
+        {
+            unsorted[unsorted_i] = in.next();
+        }
+        quicksort(unsorted,0,n-1);
+        for(int i=0; i<n;i++)
+        {
+            System.out.println(unsorted[i]);    
+        }
+    }
+}
