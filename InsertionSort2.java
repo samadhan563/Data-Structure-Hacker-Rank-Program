257f6fdf60222faa32a4e8b9494be336eabab281
Samadhan Nana Gaikwad <73218474+samadhan563@users.noreply.github.com>
2020-12-22 11:15:04 +0530

    Create InsertionSort2.java

---
+++ InsertionSort2.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class InsertionSort2 {
+
+    
+    static void insertionSort2(int n, int[] arr) {
+    
+        for (int i = 1; i < n; i++) 
+        {
+             int count=0;
+            for( int j=0 ; j<i ; j++ )
+            {
+               
+                if(arr[i]<arr[j])
+                {
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                   // count++;
+                     
+                }
+               
+            }
+         //   if(count!=0)
+                System.out.println(Arrays.toString(arr).replace("[","").replaceAll(",","").replace("]",""));
+               
+        }
+        
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        insertionSort2(n, arr);
+
+        scanner.close();
+    }
+}
