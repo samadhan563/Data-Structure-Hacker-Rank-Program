8e95de3a80bc269cac1454c3d3a2749135d2ebf5
Samadhan Nana Gaikwad <73218474+samadhan563@users.noreply.github.com>
2020-12-22 10:59:00 +0530

    Create MissingNumbers.java

---
+++ MissingNumbers.java
@@ -0,0 +1,58 @@
+import java.io.*;
+public class MissingNumbers
+{
+    public static void main(String args[]) throws IOException
+    {
+        int n,m,a[],b[],hash[],min_b;
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        n=Integer.parseInt(br.readLine());
+        a=new int[n];
+        String st=br.readLine();
+        String num[]=st.split(" ");
+        for(int i=0;i<n;i++)
+        {
+            a[i]=Integer.parseInt(num[i]);
+        }
+        m=Integer.parseInt(br.readLine());
+        b=new int[m];
+        st=br.readLine();
+        num=st.split(" ");
+        for(int i=0;i<m;i++)
+        {
+            b[i]=Integer.parseInt(num[i]);
+        }
+        min_b=find_min(b);
+        hash=new int[100];
+        for(int i=0;i<b.length;i++)
+        {
+            hash[b[i]-min_b]++;
+        }
+        for(int i=0;i<a.length;i++)
+        {
+            if((a[i]-min_b)>=0)
+            {
+                hash[a[i]-min_b]--;
+            }
+        }
+        for(int i=0;i<100;i++)
+        {
+            if(hash[i]>0)
+            {
+                System.out.print((i+min_b)+" ");
+            }
+        }
+        
+    }
+    static int find_min(int b[])
+    {
+        int min=b[0];
+        for(int i=1;i<b.length;i++)
+        {
+            if(min>b[i])
+            {
+                min=b[i];
+            }
+        }
+        return min;
+    }
+}
